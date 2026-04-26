6e6fd46d824608e5b7bc47799362637d3b6526b7
Précieux bko <precieuxboko@gmail.com>
2026-04-26 17:12:00 +0100

    add login API with bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,3 +22,29 @@ exports.register = async (req, res) => {
         res.status(500).json({ message: "Erreur hash password" });
     }
 };
+exports.login = (req, res) => {
+    const { email, password } = req.body;
+
+    const sql = "SELECT * FROM users WHERE email = ?";
+
+    db.query(sql, [email], async (err, results) => {
+        if (err) {
+            return res.status(500).json({ message: "Erreur serveur" });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ message: "Utilisateur non trouvé" });
+        }
+
+        const user = results[0];
+
+        // comparaison mot de passe
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: "Mot de passe incorrect" });
+        }
+
+        res.status(200).json({ message: "Connexion réussie", user });
+    });
+};
